0d8b508c8ba3517b841ca1e2d73cc3480715d2f0
Charles Crabtree <crabtreedcharles@gmail.com>
2017-02-18 13:44:34 -0500

    update functions

--- R/nbacolors.r
+++ R/nbacolors.r
@@ -32,13 +32,11 @@ nba.colors <- function(set=c("hawks", "hornets", "mavs", "warriors", "clippers",
 
   hornets = c("Hornets Purple" = "#1D1160",
               "Teal" = "#008CA8",
-              "White" = "#FFFFFF",
               "Gray" = "#A1A1A4")
 
   mavs = c("Mavericks Blue" = "#007DC5",
            "Mavericks Silver" = "#C4CED3",
            "Mavericks Navy" = "#20385B",
-           "White" = "#FFFFFF",
            "Black" = "#000000")
 
   warriors = c("Warriors Royal Blue" = "#006BB6",
@@ -46,78 +44,63 @@ nba.colors <- function(set=c("hawks", "hornets", "mavs", "warriors", "clippers",
 
   clippers = c("Clippers Red" = "#ED174C",
                "Royal Blue" = "#006BB6",
-               "White" = "#FFFFFF",
                "Gray" = "#A1A1A4",
                "Navy" = "#00285D")
 
   heat = c("Heat Red" = "#98002E",
            "Heat Yellow" = "#F9A01B",
-           "White" = "#FFFFFF",
            "Black" = "#000000")
 
   pelicans = c("Pelicans Blue" = "#002B5C",
                "Pelicans Gold" = "#B4975A",
-               "White" = "#FFFFFF",
                "Pelicans Red" = "#E31937")
 
   magic = c("Magic Blue" = "#007DC5",
             "Silver" = "#C4CED3",
-            "White" = "#FFFFFF",
             "Black" = "#000000")
 
   blazers = c("Blazers Red" = "#F0163A",
               "Silver" = "#B6BFBF",
-              "White" = "#FFFFFF",
               "Black" = "#000000")
 
   raptors = c("Raptors Red" = "#CE1141",
               "Raptors Silver" = "#C4CED3",
-              "White" = "#FFFFFF",
               "Black" = "#000000")
 
   celtics = c("Celtics Green" = "#008348",
               "Gold" = "#FFD700",
               "Silver" = "#C0C0C0",
-              "White" = "#FFFFFF",
               "Black" = "#000000")
 
   bulls = c("Bulls Red" = "#CE1141",
-            "White" = "#FFFFFF",
             "Black" = "#000000")
 
   nuggets = c("Nuggets Light Blue" = "#4FA8FF",
               "Nuggets Gold" = "#FFB20F",
-              "Nuggets Navy" = "#004770",
-              "White" = "#FFFFFF")
+              "Nuggets Navy" = "#004770")
 
   rockets = c("Rockets Red" = "#CE1141",
               "Silver" = "#C4CED3",
               "Mustard" = "#FDB927",
-              "White" = "#FFFFFF",
               "Black" = "#000000")
 
   lakers = c("Lakers Purple" = "#552582",
-              "Lakers Gold" = "#FDB927",
-              "White" = "#FFFFFF")
+              "Lakers Gold" = "#FDB927")
 
   bucks = c("Dark Green" = "#00471B",
             "Bucks Cream" = "#EEE1C6",
             "Light Royal Blue" = "#0077C0",
-            "White" = "#FFFFFF",
             "Black" = "#000000")
 
   knicks = c("Knicks Blue" = "#006BB6",
              "Orange" = "#F58426",
-             "Silver" = "#BEC0C2",
-             "White" = "#FFFFFF")
+             "Silver" = "#BEC0C2")
 
   sixers = c("76ers Blue" = "#006BB6",
-             "76ers Red" = "#ED174C",
-             "White" = "#FFFFFF")
+             "76ers Red" = "#ED174C")
 
   kings = c("Kings Purple" = "#724C9F",
             "Kings Silver" = "#8E9090",
-            "White" = "#FFFFFF",
             "Black" = "#000000")
 
   jazz = c("Jazz Navy" = "#002B5C",
@@ -125,23 +108,19 @@ nba.colors <- function(set=c("hawks", "hornets", "mavs", "warriors", "clippers",
            "Jazz Green" = "#00471B",
            "Jazz Gray" = "#BEC0C2")
 
-  nets = c("White" = "#FFFFFF",
-           "Black" = "#000000")
+  nets = c("Black" = "#000000")
 
   cavs = c("Cavaliers Wine" = "#860038",
            "Cavaliers Gold" = "#FDBB30",
-           "Cavaliers Navy" = "#002D62",
-           "White" = "#FFFFFF")
+           "Cavaliers Navy" = "#002D62")
 
   pistons = c("Pistons Blue" = "#006BB6",
               "Pistons Red" = "#ED174C",
-              "Pistons Navy" = "#001F70",
-              "White" = "#FFFFFF")
+              "Pistons Navy" = "#001F70")
 
   pacers = c("Pacers Blue" = "#00275D",
              "Gold" = "#FFC633",
-             "Silver" = "#BEC0C2",
-             "White" = "#FFFFFF")
+             "Silver" = "#BEC0C2")
 
   grizzlies = c("Memphis Midnight Blue" = "#23375B",
                 "Beale Street Blue" = "#6189B9",
@@ -151,7 +130,6 @@ nba.colors <- function(set=c("hawks", "hornets", "mavs", "warriors", "clippers",
   wolves = c("Timberwolves Blue" = "#005083",
              "Silver" = "#C4CED3",
              "Green" = "#00A94F",
-             "White" = "#FFFFFF",
              "Black" = "#000000")
 
   thunder = c("Thunder Blue" = "#007DC3",
@@ -162,21 +140,17 @@ nba.colors <- function(set=c("hawks", "hornets", "mavs", "warriors", "clippers",
   suns = c("Suns Orange" = "#E56020",
            "Suns Purple" = "#1D1160",
            "Suns Gray" = "#63717A",
-           "White" = "#FFFFFF",
            "Black" = "#000000")
 
   spurs = c("Silver" = "#B6BFBF",
-            "White" = "#FFFFFF",
             "Black" = "#000000")
 
   wizards = c("Navy" = "#002566",
               "Red" = "#F5002F",
-              "Silver" = "#C2CCCC",
-              "White" = "#FFFFFF")
+              "Silver" = "#C2CCCC")
 
   sonics = c("Sonics Green" = "#016332",
-             "Sonics Yellow" = "#FCE10C",
-             "White" = "#FFFFFF")
+             "Sonics Yellow" = "#FCE10C")
 
   switch(match.arg(set),
          hawks = hawks,
